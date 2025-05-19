44e77be3c4e8ad6af9de3baf2b097d36d7c9200b
Md Zahid Iqbal <109502562+void0-coder@users.noreply.github.com>
2025-05-19 19:37:16 +0530

    Update main.py
    formated code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print('Hello')
-print("Talk..")
-print("User 1: Spoke..")
-print("User 2: Spoke..:")
-print("Bye")
+if __name__ == "__main__":
+  print('Hello')
+  print("Talk..")
+  print("User 1: Spoke..")
+  print("User 2: Spoke..:")
+  print("Bye")
 
